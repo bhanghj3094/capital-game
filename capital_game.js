c4945d311c4fd95fefab3bfde3ac5dc80812284b
Hyoung Jo, Bhang <bhanghj3094@gmail.com>
2020-05-12 19:58:28 +0900

    refactor: make code more readable
     - add prettier
     - rewrite all comments
     - export to es6
     - add return to checkAnswer()

--- capital_game.js
+++ capital_game.js
@@ -1,15 +1,14 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
+// '$' is for jQuery library.
 
-// request for country, capital pair
-$.ajax(
-  "https://s3.ap-northeast-2.amazonaws.com/ec2-54-144-69-91.compute-1.amazonaws.com/country_capital_pairs_2019.csv"
+/* jQuery ajax call */
+$.get(
+  "https://s3.ap-northeast-2.amazonaws.com/ec2-54-144-69-91.compute-1.amazonaws.com/country_capital_pairs_2019.csv",
 )
   .done(response => {
-    /**
-     * Parse response into javascript array.
-     * The result is stored in windows.pairs variable.
-     */
+    /* Parse response into javascript array.
+     * The result is stored in windows.pairs variable. */
     window.pairs = [];
     const data = response.split("\r\n").slice(1);
     for (let i = 0; i < data.length; i++) {
@@ -20,6 +19,8 @@ $.ajax(
         country: country
           .split(" ")
           .map(item => {
+            /* Format country name.
+             * ex. TAIWAN (REPUBLIC OF CHINA) => Taiwan (Republic of China) */
             if (["and", "of", "the"].includes(item.toLowerCase())) {
               return item.toLowerCase();
             } else if (item.startsWith("(")) {
@@ -32,54 +33,49 @@ $.ajax(
             return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
           })
           .join(" "),
-        capital: capital
+        capital: capital,
       });
     }
 
-    /**
-     * Get only capitals array for autocompletion.
-     */
-    let current_country_capital_pair = {};
-    const capitals = []; // Array for only capitals
+    /* Initialize capitals array for newQuestion. */
+    const capitals = [];
     for (i = 0; i < window.pairs.length; i++) {
       capitals.push(window.pairs[i].capital);
     }
 
-    $(document).ready(function () {
-      // '$' is the value given for the jQuery library when it loads up.
-      // on Page Load
-      current_country_capital_pair = newQuestion();
+    /* On document loaded. */
+    $(document).ready(() => {
+      let current_country_capital_pair = newQuestion();
 
-      // enter 혹은 버튼을 눌렀을 때
+      /* On button click or 'enter' */
       $("#pr2__submit").on("click", () => {
-        checkAnswer(current_country_capital_pair);
+        current_country_capital_pair = checkAnswer(
+          current_country_capital_pair,
+        );
       });
-      $("#pr2__answer").keydown(function (event) {
+      $("#pr2__answer").keydown(event => {
         if (event.keyCode === 13) {
           $("#pr2__submit").click();
           $("#pr2__answer").autocomplete("close");
         }
       });
 
-      // Autocomplete 에서 enter 혹은 눌렀을 때, 위와 동일
+      /* In autocomplete, same as above. */
       $("#pr2__answer").autocomplete({
         source: capitals,
-        select: function (event, ui) {
-          // enter 는 위의 keyup 으로, click 은 pr2__submit 으로 위임
-          if (event.keyCode === 13) {
-          } else {
-            // input 의 value 를 선택(click or enter)로 바꿈
+        select: (event, ui) => {
+          /* 'enter' is automatically caught on above.
+           * Below is for click. */
+          if (event.keyCode !== 13) {
             $("#pr2__answer").val(ui.item.value);
             $("#pr2__submit").click();
           }
         },
-        close: function (event, ui) {
-          $("#pr2__answer").val("");
-        }
+        close: (event, ui) => $("#pr2__answer").val(""),
       });
 
-      // filtering with radio
-      $("input[type=radio][name=filter]").change(function () {
+      /* Filtering with radio. */
+      $("input[type=radio][name=filter]").change(() => {
         const filtering = this.value;
         switch (filtering) {
           case "all":
@@ -95,97 +91,72 @@ $.ajax(
             $("tr.wrong").show();
         }
       });
-
-      /* =========================================================== */
-      /* ======================== Functions ======================== */
-      /* =========================================================== */
-      /*
-       * newQuestion: country 를 불러오고, input 을 초기화한다.
-       */
-      function newQuestion() {
-        // 임의의 국가를 불러온다.
-        const country_capital_pair =
-          window.pairs[Math.floor(Math.random() * window.pairs.length)];
-        $("#pr2__question").html(country_capital_pair.country);
-
-        // input 에 자동으로 cursor blinking.
-        $("#pr2__answer")
-          .val("")
-          .focus();
-
-        current_country_capital_pair = country_capital_pair;
-        return country_capital_pair;
-      }
-
-      /*
-       * checkAnswer: check the input answer and insert to the list below.
-       */
-      function checkAnswer(current_country_capital_pair) {
-        // alert('Checking Answer..!');
-        const country = current_country_capital_pair.country;
-        const capital = current_country_capital_pair.capital;
-        const myAnswer = $("#pr2__answer").val();
-
-        // check if correct
-        const correct = myAnswer.toLowerCase() === capital.toLowerCase();
-        if (correct) {
-          // change to "All", if the filter was wrong
-          if (
-            $("input[name=filter]")
-              .filter(":checked")
-              .val() === "wrong"
-          ) {
-            $("input[value=all]").prop("checked", true);
-            $("tr").show();
-          }
-          // alert("Correct Answer!");
-          $("tr#filter").after(
-            `<tr class="correct">
-              <td>${country}</td>
-              <td>${capital}</td>
-              <td id="delete_button">
-                <i class="fas fa-check"></i>
-                <button class="delete">delete</button>
-              </td>
-            </tr>`
-          );
-        } else {
-          // change to "All", if the filter was correct
-          if (
-            $("input[name=filter]")
-              .filter(":checked")
-              .val() === "correct"
-          ) {
-            $("input[value=all]").prop("checked", true);
-            $("tr").show();
-          }
-          // alert("Wrong Answer!");
-          $("tr#filter").after(
-            `<tr class="wrong">
-              <td>${country}</td>
-              <td id="wrong"><strike>${myAnswer}</strike></td>
-              <td id="delete_button">
-                ${capital}
-                <button class="delete">delete</button>
-              </td>
-            </tr>`
-          );
-        }
-
-        // reset with a new question
-        newQuestion();
-      }
-
-      /* =========================================================== */
     });
 
-    // on Button Press => ajax 로 안에 넣을 수 없다.
-    $(document).on("click", ".delete", function () {
-      // alert("in the delete!");
-      this.closest("tr").remove();
-    });
+    /* Delete entry. */
+    $(document).on("click", ".delete", () => this.closest("tr").remove());
   })
   .fail(error => {
     alert("Could not retrieve country-capital pairs file!");
     console.error({ error });
   });
+
+/* ======================== Functions ======================== */
+/**
+ * newQuestion: clear input, show new question.
+ * @return (json) country-capital pair.
+ */
+function newQuestion() {
+  const country_capital_pair =
+    window.pairs[Math.floor(Math.random() * window.pairs.length)];
+
+  $("#pr2__question").html(country_capital_pair.country);
+  $("#pr2__answer").val("").focus();
+  return country_capital_pair;
+}
+
+/*
+ * checkAnswer: check the input answer and insert to the list below.
+ * @param (json) country-capital pair.
+ * @return (json) new country-capital pair.
+ */
+function checkAnswer(current_country_capital_pair) {
+  const country = current_country_capital_pair.country;
+  const capital = current_country_capital_pair.capital;
+  const myAnswer = $("#pr2__answer").val();
+
+  /* Check answer. */
+  const correct = myAnswer.toLowerCase() === capital.toLowerCase();
+  const correctToString = correct ? "correct" : "wrong";
+  /**
+   * Reset filter to 'All',
+   * if current filter does not contain new entry.
+   */
+  const currentFilter = $("input[name=filter]").filter(":checked").val();
+  if (currentFilter !== "All" && currentFilter !== correctToString) {
+    $("input[value=all]").prop("checked", true);
+    $("tr").show();
+  }
+  /* Insert */
+  $("tr#filter").after(
+    `<tr class=${correctToString}>
+      <td>${country}</td>
+      ${
+        correct
+          ? `<td>${capital}</td>`
+          : `<td id="wrong">
+            <strike>${myAnswer}</strike>
+          </td>`
+      }
+      <td id="delete_button">
+        ${capital}
+        <button class="delete">delete</button>
+      </td>
+    </tr>`,
+  );
+
+  /* Reset with new question. */
+  return newQuestion();
+}
+
+/* =========================================================== */
